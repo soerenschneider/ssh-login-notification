aae91e691340f710a973d04be059694967bc8861
soeren <2378192-soerenschneider@users.noreply.gitlab.com>
2020-05-25 21:15:01 +0200

    refactor: linting

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -10,10 +10,11 @@ import (
 )
 
 const (
-	nogeoresolv   = "no-resolve-geo"
-	nodnsresolv   = "no-resolve-dns"
-	telegramtoken = "telegram-token"
-	telegramId    = "telegram-id"
+	noGeoResolve     = "no-resolve-geo"
+	noDnsResolve     = "no-resolve-dns"
+	ignorePrivateIps = "ignore-private-ips"
+	telegramToken    = "telegram-token"
+	telegramId       = "telegram-id"
 )
 
 // FireUp parses the user supplied input and starts this whole mess.
@@ -25,17 +26,20 @@ func FireUp() *cobra.Command {
 	viper.AddConfigPath("$HOME/.config/ssh-login-notification")
 
 	err := viper.ReadInConfig()
-	if err != nil {
-		log.Warnf("Couldn't read conf: %v", err)
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		log.Errorf("Didn't find config file: %v", err)
+	} else if err != nil {
+		log.Errorf("Couldn't read config file: %v", err)
 	}
 
 	main := &cobra.Command{
 		Use: "ssh-login-notification",
 	}
 
-	main.Flags().BoolP(nogeoresolv, "g", viper.GetBool(strings.Replace(nogeoresolv, "-", "_", -1)), "Do NOT lookup ip geo information")
-	main.Flags().BoolP(nodnsresolv, "d", viper.GetBool(strings.Replace(nodnsresolv, "-", "_", -1)), "Do NOT lookup dns information")
-	main.Flags().StringP(telegramtoken, "t", viper.GetString(strings.Replace(telegramtoken, "-", "_", -1)), "Telegram bot token")
+	main.Flags().BoolP(noGeoResolve, "g", viper.GetBool(strings.Replace(noGeoResolve, "-", "_", -1)), "Do NOT lookup ip geo information")
+	main.Flags().BoolP(noDnsResolve, "d", viper.GetBool(strings.Replace(noDnsResolve, "-", "_", -1)), "Do NOT lookup dns information")
+	main.Flags().BoolP(ignorePrivateIps, "p", viper.GetBool(strings.Replace(ignorePrivateIps, "-", "_", -1)), "Do nothing when a private IP is detected")
+	main.Flags().StringP(telegramToken, "t", viper.GetString(strings.Replace(telegramToken, "-", "_", -1)), "Telegram bot token")
 	main.Flags().Int64P(telegramId, "i", viper.GetInt64(strings.Replace(telegramId, "-", "_", -1)), "Telegram message id")
 
 	main.Run = func(cmd *cobra.Command, args []string) {
@@ -50,13 +54,15 @@ func FireUp() *cobra.Command {
 func parseOptions(cmd *cobra.Command) internal.Options {
 	options := internal.Options{}
 
-	noGeoResolve, _ := cmd.Flags().GetBool(nogeoresolv)
+	noGeoResolve, _ := cmd.Flags().GetBool(noGeoResolve)
 	options.GeoLookup = !noGeoResolve
 
-	noDnsLookup, _ := cmd.Flags().GetBool(nodnsresolv)
+	noDnsLookup, _ := cmd.Flags().GetBool(noDnsResolve)
 	options.DnsLookup = !noDnsLookup
 
-	options.TelegramToken, _ = cmd.Flags().GetString(telegramtoken)
+	options.IgnorePrivateIps, _ = cmd.Flags().GetBool(ignorePrivateIps)
+
+	options.TelegramToken, _ = cmd.Flags().GetString(telegramToken)
 	options.TelegramId, _ = cmd.Flags().GetInt64(telegramId)
 
 	return options
